7cfa90a30839a162a1a324580b20c54d2b1059fe
SHUBHAM CHAUHAN <shubhamchauhanpoly@gmail.com>
2024-12-30 00:40:37 +0530

    Message Container UI

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,12 @@
+import MessageContainer from "../component/messages/MessageContainer";
 import Sidebar from "../component/sidebar/Sidebar";
 
 const Home = () => {
   return (
     <div className="flex sm:h[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-200">
       <Sidebar />
+
+      <MessageContainer/>
     </div>
   );
 };
