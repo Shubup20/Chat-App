165160a280b3908eab615544158780b3de4698a4
SHUBHAM CHAUHAN <shubhamchauhanpoly@gmail.com>
2024-12-29 18:56:23 +0530

    Signup Ui

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -37,15 +37,17 @@ const Login = () => {
           </div>
 
           <div>
-            <button className="btn btn-block btn-sm hover:bg-sky-700  mt-5 bg-blue-700 font-bold text-white">
+            <button className="btn btn-block btn-sm hover:bg-blue-900  mt-5 bg-blue-700 font-bold text-white">
               Login
             </button>
           </div>
 
-          <Link
-            to={"/signup"}
-            className="text-sm hover:underline hover:text-blue-600 mt-2  inline-block"
-          >{`Don't have an account? signup`}</Link>
+          <Link to={"/signup"} className="text-sm  mt-2  inline-block">
+            {`Don't have an account? `}
+            <span className="hover:underline text-blue-600  hover:text-blue-800">
+              Signup
+            </span>
+          </Link>
         </form>
       </div>
     </div>
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,5 +1,73 @@
+import { Link } from "react-router-dom";
+import GenderCheckbox from "../component/GenderCheckbox";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-200">
+        <h1 className="flex gap-2 text-3xl font-semibold text-center text-slate-700">
+          <p> Signup to</p>
+          <span className="text-blue-700"> Chat Application</span>
+        </h1>
+
+        <form>
+          <div>
+            <label className="label p-2 ">
+              <span className="text-base label-text ">Username</span>
+            </label>
+
+            <input
+              type="text"
+              placeholder="Enter Username"
+              className="w-full input input-bordered
+            h-10"
+            />
+          </div>
+
+          <div>
+            <label className="label p-2 ">
+              <span className="text-base label-text ">Email</span>
+            </label>
+
+            <input
+              type="text"
+              placeholder="shub@gmail.com"
+              className="w-full input input-bordered
+            h-10"
+            />
+          </div>
+
+          <div>
+            <label className="label p-2 ">
+              <span className="text-base label-text ">Password</span>
+            </label>
+
+            <input
+              type="text"
+              placeholder="Enter Your Password"
+              className="w-full input input-bordered
+            h-10"
+            />
+          </div>
+
+          <GenderCheckbox />
+
+          <div>
+            <button className="btn btn-block btn-sm hover:bg-blue-900  mt-2 bg-blue-700 font-bold text-white">
+              Signup
+            </button>
+          </div>
+
+          <Link to={"/login"} className="text-sm  mt-2  inline-block">
+            {`Already have an account? `}
+            <span className="hover:underline text-blue-600  hover:text-blue-800">
+              Login
+            </span>
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
