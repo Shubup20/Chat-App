f144b4355142566eb220583f17c96a9734002e7d
SHUBHAM CHAUHAN <shubhamchauhanpoly@gmail.com>
2025-01-04 00:29:02 +0530

    auth

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,6 +1,6 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 /* eslint-disable react-refresh/only-export-components */
+/* eslint-disable no-unused-vars */
 import { Children, createContext, useContext, useState } from "react";
 
 export const AuthContext = createContext();
@@ -10,12 +10,18 @@ export const useAuthContext = () => {
 };
 
 export const AuthContextProvider = ({ children }) => {
-  const [authUser, setAuthUser] = useState(
-    JSON.parse(localStorage.getItem("user")) || null
-  );
+  const [authUser, setAuthUser] = useState(() => {
+    const user = localStorage.getItem("user");
+    return user ? JSON.parse(user) : null;
+  });
+
+  const logout = () => {
+    localStorage.removeItem("user");
+    setAuthUser(null);
+  };
 
   return (
-    <AuthContext.Provider value={{ authUser, setAuthUser }}>
+    <AuthContext.Provider value={{ authUser, setAuthUser, logout }}>
       {children}
     </AuthContext.Provider>
   );
